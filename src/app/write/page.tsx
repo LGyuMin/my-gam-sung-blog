80200c55af8267fb40a8207b2a6bf51104d7405c
규.이규민 <gmlee@lunasoft.co.kr>
2023-12-15 14:19:55 +0900

    write 페이지 구현 작업

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,10 +1,28 @@
+"use client"
+
+import { useState } from 'react'
+import ReactQuill from 'react-quill';
+
 import styles from '@/app/page.module.scss'
+import WriteStyle from './write.module.scss'
+import 'react-quill/dist/quill.snow.css'
 
 export default function Write() {
+    
     return (
         <>
             <div className={styles.title_box}>
                 <h2 className='font_play_fair'>Write</h2>
+            </div>
+            <div className={`${WriteStyle.editor_box} font_noto_sans`}>
+                <input type="text" placeholder='제목을 입력하세요.' className={WriteStyle.title_input} />
+                <ReactQuill placeholder='내용을 입력하세요.'>
+                    <div className={WriteStyle.editor_area}/>
+                </ReactQuill>
+                <div className={WriteStyle.editor_btn_box}>
+                    <button>save</button>
+                    <button>cancle</button>
+                </div>
             </div>
         </>
     )
